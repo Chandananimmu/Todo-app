4f073484c67ad760693ffef80872c6a9a7976a4e
Chandana S <chandanas@brimacdt-043.brillio.com>
2019-02-22 15:38:10 +0530

    CRUD complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+//= require jquery-ui
+//= require jquery
+//= require jquery_ujs
+
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
-class TodosController < ActionController::Base
+class TodosController < ApplicationController
+    before_action :set_todo, only: [:edit, :update, :show, :destroy]
     def new
        @todo=Todo.new
     end
@@ -13,13 +14,13 @@ class TodosController < ActionController::Base
      end
     end
     def show
-        @todo = Todo.find(params[:id])
+       
     end
      def edit
-     @todo=Todo.find(params[:id])
+    
      end
      def update
-    @todo=Todo.find(params[:id]) 
+  
     if @todo.update(todo_params)
         flash[:notice]= "Todo was updated successfully!" 
       redirect_to todo_path(@todo)
@@ -30,7 +31,15 @@ class TodosController < ActionController::Base
      def index
         @todos=Todo.all
      end
+     def destroy
+        @todo.destroy
+        flash[:notice]= "Todo was deleted successfully!" 
+        redirect_to todos_path
+     end
     private 
+    def set_todo
+        @todo=Todo.find(params[:id])
+    end
     def todo_params
         params.require(:todo).permit(:name,:description)
     end
